e519f2b4b1918c2846930ee5b4b2ed4106f16de3
Janusz Chudzynski <janusz@izotx.com>
2018-09-14 16:07:56 -0500

    quick sort with partitioning implemeted

--- algos/VariousAlgosPlayground.playground/Contents.swift
+++ algos/VariousAlgosPlayground.playground/Contents.swift
@@ -2,7 +2,141 @@
 
 import UIKit
 
-var str = "Hello, playground"
+func quicksort(_ a: inout [Int] ,left:Int, right:Int) -> [Int]{
+    guard left < right else{
+        return a
+    }
+    //partition array
+    let pivot = partition(&a, left: left, right: right)
+    
+    quicksort(&a, left: left, right: pivot - 1)
+    quicksort(&a, left: pivot, right: right)
+    
+    return a
+}
+
+
+// Keeping track of subarrays a low ... i < pivot
+/*
+    a[low...i] contains all values <= pivot moving everything less than pivot before i
+    a[i+1...j-1] contains all values > pivot
+    a[j...high-1] are values we haven't looked at yet
+    a[high] is the pivot value
+    first iteration:
+    l = 0
+    j = 0
+    right = 5
+*/
+
+func partition(_ a:inout[Int], left:Int, right:Int)->Int{
+    let pivot = a[right]
+    var l = left
+    let h = right
+    for j in left ..< right{
+        if(a[j]) <= pivot{
+            //Putting All Values less than pivot in first part of the array before i
+            (a[l],a[j]) = (a[j],a[l])
+            l += 1
+        }
+    }
+         // put pivot in its right place
+        (a[l],a[right]) = (a[right],a[l])
+    return l
+}
+
+
+var k1 = [12,2,5,7,15]
+
+quicksort(&k1, left: 0, right: k1.count - 1)
+k1
+
+
+
+func quicksort1( _ array: inout  [Int])->[Int]{
+    
+    guard array.count > 1 else{ return array}
+    
+    //pick pivot
+    let pivot = array[array.count / 2]
+
+    //less than pivot
+    var l = array.filter{$0 < pivot}
+    var eq = array.filter{$0 == pivot}
+    var m = array.filter{$0 > pivot}
+
+    //let a = l + eq + m
+   return quicksort1(&l) + eq + quicksort1(&m)
+}
+
+var k = [12,2,5,7,1]
+quicksort1(&k)
+
+//
+//func quicksort1(_ array:[Int] ,left:Int, right:Int) -> [Int]{
+//    var result = array
+//    //Base Condition
+//    if(left >= right){
+//        return result
+//    }
+//
+//    let pivot = array[(left + right) / 2]
+//    let r = partition(result, left: left, right: right, pivot: pivot)
+//    let index = r.0
+//    let a = r.1
+//
+//    result = a
+//    result = quicksort(&result, left: left, right: index - 1)
+//    result = quicksort(&result, left: index + 1, right:right)
+//
+//    return result
+//}
+
+//
+//func partition(_ array:[Int] ,left _left:Int, right _right:Int,pivot:Int) -> (Int, [Int]){
+//    var result = array
+//    var left = _left
+//    var right = _right
+//
+//    while left <= right {
+//        while(array[left] < pivot){
+//            left += 1
+//        }
+//
+//        while(array[right] > pivot){
+//            right -= 1
+//        }
+//
+//        if(left <= right){
+//            //swap
+//            left += 1
+//            right += 1
+//            result = swap(array,left,right)
+//        }
+//    }
+//    return (left, result)
+//}
+
+func swap(_ _array:[Int], _ left:Int, _ right: Int)->[Int]{
+    var array = _array
+    let temp = array[right]
+    array[right] = array[left]
+    array[left] = temp
+    return array
+}
+
+
+var array = [9,3,5,1,7,1]
+//mergesort(array)
+
+//quicksort(array, left: 0, right: array.count - 1)
+
+
+//(array[0],array[1]) = (array[1],array[0])
+//array
+
+
+
+
 
 
 func mergesort(_ array:[Int])->[Int]{
@@ -37,7 +171,7 @@ func merge(left:[Int],right:[Int])->[Int]{
         else{
             array.append(right[leftIndex] )
             array.append(left[leftIndex] )
-
+            
             rightIndex += 1
             leftIndex += 1
             
@@ -56,72 +190,3 @@ func merge(left:[Int],right:[Int])->[Int]{
     
     return array
 }
-
-
-func quicksort(array:[Int]){
-    
-}
-
-func quicksort(_ array:[Int] ,left:Int, right:Int) -> [Int]{
-    var result = array
-    //Base Condition
-    if(left >= right){
-        return result
-    }
-    
-    let pivot = array[(left + right) / 2]
-    let r = partition(result, left: left, right: right, pivot: pivot)
-    let index = r.0
-    let a = r.1
-    
-    result = a
-    result = quicksort(result, left: left, right: index - 1)
-    result = quicksort(result, left: index + 1, right:right)
-
-    return result
-}
-
-
-func partition(_ array:[Int] ,left _left:Int, right _right:Int,pivot:Int) -> (Int, [Int]){
-    var result = array
-    var left = _left
-    var right = _right
-
-    while left <= right {
-        while(array[left] < pivot){
-            left += 1
-        }
-        
-        while(array[right] > pivot){
-            right -= 1
-        }
-        
-        if(left <= right){
-            //swap
-            left += 1
-            right += 1
-            result = swap(array,left,right)
-        }
-    }
-    return (left, result)
-}
-
-func swap(_ _array:[Int], _ left:Int, _ right: Int)->[Int]{
-    var array = _array
-    let temp = array[right]
-    array[right] = array[left]
-    array[left] = temp
-    return array
-}
-
-
-var array = [9,3,5,1,7,1]
-//mergesort(array)
-
-quicksort(array, left: 0, right: array.count - 1)
-
-
-(array[0],array[1]) = (array[1],array[0])
-array
-
-

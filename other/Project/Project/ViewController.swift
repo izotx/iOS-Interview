79d58d8e39093e6f1416b4fd92b61954b1966c43
Janusz Chudzynski <janusz@izotx.com>
2018-11-11 17:59:28 -0600

    More Interview Projects

---
+++ other/Project/Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Janusz Chudzynski on 11/10/18.
+//  Copyright © 2018 izotx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
